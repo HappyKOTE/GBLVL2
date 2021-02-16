5a19e4cae48d7bb1571aa08e2a3d1e05e639c5b6
HappyKOTE <vpavlov.moscow@gmail.com>
2021-02-16 17:43:27 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,3 @@
-/*const http = require('http').createServer(handler);
-const fs = require('fs');
-
-function handler(req, res) {
-    console.log(req.url);
-    let body = null;
-    if (req.url != '/index.html' || req.url != '/') {
-        const myURL = res.url.pathname;
-        body = fs.readFileSync(`./public${myURL}`, 'utf8');
-    } else {
-        body = fs.readFileSync('./public/index.html', 'utf8');
-    }
-    res.end(body);
-}
-
-const port = process.env.PORT || 3000;
-http.listen(port);
-console.log(`Server started on port ${port}!`);*/
-
 let express = require('express'),
     app = express();
 app.use(express.static(__dirname + '/public'));
