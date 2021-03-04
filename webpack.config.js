72e3a6a6926d83e7482c172ec71412f7f070d760
HappyKOTE <vpavlov.moscow@gmail.com>
2021-03-05 01:05:18 +0300

    new

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path')
+
+module.exports = {
+    entry: './src/shop.js',
+    output: {
+        path: path.resolve(__dirname, 'public/js'),
+        filename: "script.js",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: [
+                    { loader: 'babel-loader' },
+                ],
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    { loader: 'style-loader' },
+                    { loader: 'css-loader' },
+                    { loader: 'sass-loader' }
+                ],
+            },
+        ],
+    },
+}
